d68f752037099aa05ccf9633bc581736e221fa81
yamadeey <daiinzm1130@icloud.com>
2021-06-23 11:23:33 +0900

    登校時のバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,6 +6,7 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
+  validates :image, presence: true
   
 
   # validates :content, presence: true, unless: :was_attached?
